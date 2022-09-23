70accef5ca496c7597dadb220623ff6497444d39
Melin Kan <melin.kan@outlook.com>
2022-09-23 22:02:00 +0300

    Create TodoContext

---
+++ src/contexts/TodoContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useState, useContext } from "react";
+
+export const TodoContext = createContext();
+export const useTodoContext = () => useContext(TodoContext);
+
+export const TodoProvider = (props) => {
+  const savedTodos = JSON.parse(localStorage.getItem("todos"));
+  const [todos, setTodos] = useState(savedTodos || []);
+  const [form, setForm] = useState({ todo: "" });
+  console.log(todos);
+  return (
+    <TodoContext.Provider
+      value={{
+        todos,
+        setTodos,
+        form,
+        setForm,
+      }}
+    >
+      {props.children}
+    </TodoContext.Provider>
+  );
+};
